8bb5d67f7284f4a0f6991640752d82100c726d53
emmajane1313 <admin@digitalax.xyz>
2023-09-05 12:18:59 -0400

    stripe update

---
+++ src/pages/tos.tsx
@@ -0,0 +1,126 @@
+import { FunctionComponent } from "react";
+
+const ToS: FunctionComponent = (): JSX.Element => {
+  return (
+    <div className="relative w-full h-screen flex flex-col items-center justify-start overflow-x-hidden text-slate-200 bg-black">
+      # Terms of Service for Coin Op
+      <br />
+      _Last Updated: September 5, 2023_
+      <br /> <br />
+      ## Introduction
+      <br />
+      Welcome to Coin Op. By opting for this platform, you are aligning with a
+      vision of personal autonomy, open-source technology, and public domain
+      creativity. These Terms of Service ("ToS") govern the use of various
+      decentralized services ("Services").
+      <br /> <br />
+      ## Acceptance of Terms
+      <br />
+      By accessing or using our Services, you agree to follow and be bound by
+      these terms, making them a mutual agreement between you and Coin Op.
+      <br /> <br />
+      ## Our Ethos: Decentralization at its Core
+      <br />
+      Coin Op is built on the principles of the cypherpunk movement, emphasizing
+      personal autonomy, privacy, and decentralized technology. Our platform
+      leverages the cutting edge in decentralization—from asset storage to
+      opt-in social media integration via Lens Protocol.
+      <br /> <br />
+      ## Our Services
+      <br />
+      We offer a decentralized platform for creating and purchasing custom and
+      pre-made streetwear and street art prints, facilitated by AI diffusion
+      models. We also provide "prerolls" of prints and apparel.
+      <br /> <br />
+      ### User Requirements
+      <br />
+      - You must be at least 13 years old to use our Services. - You agree to
+      provide accurate information during transactions.
+      <br /> <br />
+      ### User-Generated Content: Navigating the Frontier of Public Domain
+      <br />
+      While the legal status of AI-generated works remains a subject of ongoing
+      debate and litigation, Coin Op takes a proactive stance by releasing all
+      such content into the public domain upon creation. This aligns with the
+      ethos underpinning copyright law, which aims to foster scientific progress
+      and artistic expression, rather than monopolizing the basic elements of
+      creativity. By using the Services, you agree to contribute your
+      creations—whether AI-generated or otherwise—to the public domain. This
+      liberates these works from all copyright and proprietary restrictions,
+      allowing anyone to use them for any purpose. You thereby waive all
+      copyright and related rights in your contributions to the fullest extent
+      possible under the law, enriching a shared digital commons.
+      <br /> <br />
+      ### Payments: Flexibility and Autonomy
+      <br />
+      For debit and credit card transactions, we currently use Stripe as our
+      primary payment processor, although we reserve the right to switch or add
+      payment processing providers. In the spirit of financial autonomy and
+      decentralization, we also accept payments in various EVM-based tokens,
+      including $ETH, $USDT, $MONA, and $MATIC. To make a token payment, you'll
+      need a self-custody wallet like Rainbow, Metamask, or Coinbase Wallet. By
+      using these payment methods, you agree to the respective terms and
+      conditions of these payment processors and wallets.
+      <br /> <br />
+      #### Tax Implications for Token Payments
+      <br />
+      If you choose to make payments using EVM-based tokens, you are solely
+      responsible for reporting and paying any applicable taxes associated with
+      such transactions. Different jurisdictions have varying tax rules relating
+      to the use of tokens for payments, and it's your responsibility to
+      understand and comply with these. Coin Op is not responsible for reporting
+      or paying taxes on your behalf for token-based transactions.
+      <br /> <br />
+      ## Refunds and Returns
+      <br />
+      All sales are final, given the custom nature of many of our products.
+      Exceptions may be made at our discretion for damaged or defective items,
+      which must be reported within 7 days of receipt.
+      <br /> <br />
+      ## Security: Decentralized and Robust
+      <br />
+      In line with our commitment to decentralization, we employ Programmable
+      Key Pairs (PKP) and Lit Actions decentralized encryption technology,
+      ensuring the utmost security for your data.
+      <br /> <br />
+      ## Liability
+      <br />
+      Coin Op shall not be liable for any indirect, incidental, special,
+      consequential, or punitive damages, including but not limited to loss of
+      profits, data, use, goodwill, or other intangible losses, resulting from
+      your access to or use of the Service.
+      <br /> <br />
+      ## Privacy Policy
+      <br />
+      Please refer to our Privacy Policy for a detailed understanding of our
+      data collection, usage, and protection measures.
+      <br /> <br />
+      ## Termination
+      <br />
+      We reserve the right to suspend or terminate your access to the Services
+      for behavior we deem detrimental to other users, us, or third parties.
+      <br /> <br />
+      ## Changes to ToS
+      <br />
+      These ToS may be updated in accordance with advancements in decentralized
+      technology and ethical considerations.
+      <br /> <br />
+      ## Dispute Resolution
+      <br />
+      Any disputes arising from these ToS will be governed by the laws of the
+      State of New York.
+      <br /> <br />
+      ## Contact Us
+      <br />
+      For questions about these ToS, reach out to us at
+      support@themanufactory.xyz.
+      <br /> <br />
+      ## Compliance with Laws
+      <br />
+      You are responsible for adhering to all applicable laws during your
+      engagement with our Services.`
+    </div>
+  );
+};
+
+export default ToS;
