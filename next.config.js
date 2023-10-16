faf960be72a20bf582741af8e08a8670957d98d0
emmajane1313 <admin@digitalax.xyz>
2023-10-16 18:57:52 -0400

    buffer'

--- next.config.js
+++ next.config.js
@@ -24,7 +24,7 @@ const nextConfig = {
 
     config.plugins.push(
       new webpack.ProvidePlugin({
-        Buffer: ["buffer", "Buffer"],
+        Buffer: ['buffer', 'Buffer'],
       })
     );
 
