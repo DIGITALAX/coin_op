e2593993ebacdea7c51a1c1fe0a78c23c9fccc9f
emmajane1313 <admin@digitalax.xyz>
2024-01-03 01:06:36 -0500

    clean collect

--- lib/lens/helpers/cleanCollect.ts
+++ lib/lens/helpers/cleanCollect.ts
@@ -1,5 +1,4 @@
-import { OpenActionModuleInput } from "@/components/Common/types/generated";
-
+import { OpenActionModuleInput } from "../../../src/components/Common/types/generated";
 const cleanCollect = (
   openActionModules: OpenActionModuleInput[]
 ): OpenActionModuleInput[] => {
@@ -19,22 +18,20 @@ const cleanCollect = (
         newSimpleCollectOpenAction;
     }
 
-    if (
-      firstModule.collectOpenAction?.simpleCollectOpenAction?.hasOwnProperty(
-        "amount"
-      ) &&
-      (!firstModule.collectOpenAction.simpleCollectOpenAction.amount?.hasOwnProperty(
-        "value"
-      ) ||
-        !firstModule.collectOpenAction?.simpleCollectOpenAction?.amount?.hasOwnProperty(
-          "currency"
-        )) &&
-      parseFloat(
-        firstModule.collectOpenAction?.simpleCollectOpenAction?.amount?.value ||
-          ""
-      ) <= 0
-    ) {
-      delete firstModule.collectOpenAction?.simpleCollectOpenAction?.amount;
+    let simpleCollect = firstModule.collectOpenAction?.simpleCollectOpenAction;
+    if (simpleCollect && simpleCollect.hasOwnProperty("amount")) {
+      const amount = simpleCollect.amount;
+      if (
+        !amount?.value ||
+        !amount?.currency ||
+        parseFloat(amount?.value || "") <= 0
+      ) {
+        const { amount, ...restOfSimpleCollect } = simpleCollect;
+        simpleCollect = restOfSimpleCollect;
+      }
+    }
+    if (firstModule.collectOpenAction) {
+      firstModule.collectOpenAction.simpleCollectOpenAction = simpleCollect;
     }
 
     openActionModules[0] = firstModule;
